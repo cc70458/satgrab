c6f9e0e2aaf7f1b5f1cacbb0fbee3d559fc53d46
Colin <cc70458@gmail.com>
2020-10-27 11:14:03 -0500

    Update of satgrab to point to a new NOAA source
    Ok - so with hurricane Zeta bearing down - This is my 15 min update of my original 15 min program with a new data source and method - Data at the source updates every 10 mins instead of every 30 and the picture is much better.

--- satgrab.py
+++ satgrab.py
@@ -1,30 +1,32 @@
-from ftplib import FTP
+import re
 import wget
 import os
 import ctypes
 from pathlib import Path
 
-ftp = FTP('ftp.nnvl.noaa.gov')
-ftp.login()
-ftp.cwd('GOES')
-ftp.cwd('GER')
-files = []
-ftp.dir(files.append)
+webfile = Path("d:/wallpaper/conus.php")
+if webfile.is_file():
+    res = os.system('del *.php')
 
-ftp.quit()
-print(len(files))
-filename = files[-1]
-filename = filename[-29:]
-print(filename)
+url = 'https://www.star.nesdis.noaa.gov/GOES/conus.php?sat=G16'
+wget.download(url)
+print ('Choosing the latest Satellite Image')
+pattern = re.compile("[0-9]+_GOES16-ABI-CONUS-GEOCOLOR-5000x3000.jpg")
+textfile = open("conus.php", 'r')
+filetext = textfile.read()
+textfile.close()
+match = str(re.findall(pattern,filetext))
+filename = str(match[:-2][2:])
+header = "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/CONUS/GEOCOLOR/" + str(match[:-2][2:])
+print(header)
 
-url = "ftp://ftp.nnvl.noaa.gov/GOES/GER/"+filename
-print(url)
-
-my_file = Path('d:/wallpaper/wallpaper.jpg')
+my_file = Path("d:/wallpaper/wallpaper.jpg")
 if my_file.is_file():
     os.remove('d:/wallpaper/wallpaper.jpg')
-wget.download(url, 'D:/wallpaper/wallpaper.jpg')
+print ('Downloading the file')    
+wget.download(header, 'D:/wallpaper/wallpaper.jpg')
 
+print ('Changing the Wallpaper to the lastest downloaded file')
 drive = "D:\\"
 folder = "wallpaper"
 image = "wallpaper.jpg"
